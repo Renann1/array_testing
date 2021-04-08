3712c79f77af6ccd0463dc69f1457923122be862
Renann Santos <69852026+Renann1@users.noreply.github.com>
2021-04-08 11:48:44 -0300

    Update MinhaClasse.java

--- MinhaClasse.java
+++ MinhaClasse.java
@@ -2,7 +2,7 @@ package testepacote;
 public class MinhaClasse {
     public static void main(String[] args){
         String nomes[] = new String[3];
-        //atribuir valor a cada posição do arrey, de 0 a 2
+        //atribuir valor a cada posição do array, de 0 a 2
         nomes[0] = "Fulano";
         nomes[1] = "Beltrano";
         nomes[2] = "Sicrano";
